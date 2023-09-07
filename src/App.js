2e8e7bb15b40045ff4cc42e9f26a4abb7445d069
Daniel Mogil <danmogil@gmail.com>
2023-09-06 21:04:54 -0400

    add mongo client & auth

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import { ChatContextProvider } from './context/chatContext';
 import SideBar from './components/SideBar';
 import ChatView from './components/ChatView';
-import { useEffect, useState } from 'react';
-import Modal from './components/Modal';
-import Setting from './components/Setting';
+
+import * as Realm from 'realm-web';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+
+const app = new Realm.App(process.env.MONGO_APP_ID);
+
+async function getValidAccessToken() {
+  if (!app.currentUser) {
+    await app.logIn(Realm.Credentials.anonymous());
+  } else {
+    // An already logged in user's access token might be stale. Tokens must be refreshed after
+    // 30 minutes. To guarantee that the token is valid, we refresh the user's access token.
+    await app.currentUser.refreshAccessToken();
+  }
+  return app.currentUser.accessToken;
+}
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: process.env.GRAPHQL_URI,
+    fetch: async (uri, options) => {
+      const accessToken = await getValidAccessToken();
+      options.headers.Authorization = `Bearer ${accessToken}`;
+      return fetch(uri, options);
+    },
+  }),
+  cache: new InMemoryCache(),
+});
+
+function insert_log(prompt, model_response) {
+  const user_id = app.currentUser.id;
+  // const timestamp
+  // TODO
+}
 
 const App = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  useEffect(() => {
-    const apiKey = window.localStorage.getItem('api-key');
-    if (!apiKey) {
-      setModalOpen(true);
-    }
-  }, []);
   return (
-    <ChatContextProvider>
-      <Modal title='Setting' modalOpen={modalOpen} setModalOpen={setModalOpen}>
-        <Setting modalOpen={modalOpen} setModalOpen={setModalOpen} />
-      </Modal>
-      <div className='flex transition duration-500 ease-in-out'>
-        <SideBar />
-        <ChatView />
-      </div>
-    </ChatContextProvider>
+    <ApolloProvider client={client}>
+      <ChatContextProvider>
+        <div className='flex transition duration-500 ease-in-out'>
+          <SideBar />
+          <ChatView post_log={insert_log} />
+        </div>
+      </ChatContextProvider>
+    </ApolloProvider>
   );
 };
 
