4c3d7a89b965cdde3806ccac6dc49b64ae0f8eae
bash <44552816+gutbash@users.noreply.github.com>
2023-09-25 20:18:13 -0400

    change timestamp import from firestore to realtime

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import ChatView from './components/ChatView';
 
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
-import { getDatabase, ref, push } from 'firebase/database';
-import { serverTimestamp } from 'firebase/firestore';
+import { getDatabase, ref, push, serverTimestamp } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBhsjVuPP_njleOaq68JRQ3BNbbdzSrMZc',
