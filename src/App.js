0b37cb425d60b3ba45689b4fd816b950f527526a
bash <44552816+gutbash@users.noreply.github.com>
2023-09-25 20:41:45 -0400

    add success logging to writeLog

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ function writeLog(prompt, response, rating) {
   }).catch(error => {
     console.error('Failed to write log:', error);
   });
+  console.log('Wrote log');
 }
 
 const App = () => {
